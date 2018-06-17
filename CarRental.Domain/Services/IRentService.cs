8ec3d73590ff848069fd2b9d41544647d6a4d600
Chris <christobal.calderon@netsolution.nu>
2018-06-17 21:26:48 +0200

    more tests

--- CarRental.Domain/Services/IRentService.cs
+++ CarRental.Domain/Services/IRentService.cs
@@ -6,7 +6,7 @@ namespace CarRental.Domain.Services
 {
     public interface IRentService
     {
-        Task<int> RentAsync(string licensePlate, string ssn, DateTime startOfRent, int startOfCurrentMeter);
+        Task<int> RentAsync(string licensePlate, string personalIdentityNumber, DateTime startOfRent, int startOfCurrentMeter);
         Task<Rent> ReturnAsync(int id, DateTime endOfRent, int endOfCurrentMeter);
     }
 }
